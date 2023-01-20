2ec0744e6d377b42a218a72280720fd721edb3a0
BillOchieng <ochieng01@allegheny.edu>
2023-01-20 16:19:01 -0500

    add info to week1 course materials

--- Week1/src/songs2.py
+++ Week1/src/songs2.py
@@ -1,6 +1,15 @@
 def compute():
-	songs = [1,2,5,4,3]
-	# add your code here to compute min
-	
+    songs = [1, 2, 5, 4, 3]
+    # add your code here to compute min
+    temp = songs[0]
+    res = 1
+    for i in range(len(songs)):
+        if songs[i] > temp:
+            temp = songs[i]
+            res = i + 1
+    print("The least played song", temp)
+    print("Play counts ", res)
+
+
 if __name__ == "__main__":
     compute()
